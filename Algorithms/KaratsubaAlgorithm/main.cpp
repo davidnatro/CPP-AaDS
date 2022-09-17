b1df774dce0fb10d6b4b34c55043ba95b71dd4ed
Lone Warden <davidnatro03@gmail.com>
2022-09-18 01:38:45 +0300

    multiplication algorithm

--- Algorithms/KaratsubaAlgorithm/main.cpp
+++ Algorithms/KaratsubaAlgorithm/main.cpp
@@ -61,79 +61,167 @@ uint64_t karatsuba(uint64_t x, uint64_t y) {
 using std::cin;
 using std::cout;
 
-int main() {
-    std::string x, y;
-    // cin >> x >> y;
-    std::ifstream f_in("../input.txt");
-    while (f_in >> x >> y) {
-        uint64_t result = 0;
+// int main() {
+//     std::string x, y;
+//     cin >> x >> y;
+//
+//     uint64_t result = 0;
+//
+//     uint64_t temp_res;
+//
+//     uint64_t y_pow;
+//     uint64_t x_pow = 0;
+//
+//     for (int i = x.size() - 1; i >= 0; --i) {
+//         uint64_t v_ume = 0;
+//         y_pow = 0;
+//         for (int j = y.size() - 1; j >= 0; --j) {
+//             v_ume *= pow(10, y_pow);
+//
+//             temp_res =
+//                 (static_cast<int>((y[j] - '0')) * static_cast<int>((x[i] - '0')) *
+//                      static_cast<uint64_t>(pow(10, y_pow)) * static_cast<uint64_t>(pow(10,
+//                      x_pow)) +
+//                  v_ume);
+//             if (temp_res >= 10) {
+//                 v_ume = static_cast<uint64_t>(
+//                     floor(temp_res / static_cast<uint64_t>((pow(10, y_pow + 1)))));
+//                 temp_res %= static_cast<uint64_t>(pow(10, y_pow + 1));
+//             }
+//             result += temp_res;
+//             ++y_pow;
+//             if (j == 0) {
+//                 result += v_ume * static_cast<uint64_t>(pow(10, y_pow));
+//             }
+//         }
+//         ++x_pow;
+//     }
+//
+//     cout << result;
+//
+//     return 0;
+// }
 
-        uint64_t temp_res;
+void mult(std::string x, std::string y) {
+    uint64_t result = 0;
 
-        uint64_t y_pow;
-        uint64_t x_pow = 0;
+    uint64_t temp_res;
 
-        for (int i = x.size() - 1; i >= 0; --i) {
-            uint64_t v_ume = 0;
-            y_pow = 0;
-            for (int j = y.size() - 1; j >= 0; --j) {
-                v_ume *= pow(10, y_pow);
+    uint64_t y_pow;
+    uint64_t x_pow = 0;
 
-                temp_res =
-                    (static_cast<uint64_t>((y[j] - '0')) * static_cast<uint64_t>((x[i] - '0')) *
-                         static_cast<uint64_t>(pow(10, y_pow)) *
-                         static_cast<uint64_t>(pow(10, x_pow)) +
-                     v_ume);
-                if (temp_res >= 10) {
-                    v_ume = static_cast<uint64_t>(
-                        floor(temp_res / static_cast<uint64_t>((pow(10, y_pow + 1)))));
-                    temp_res %= static_cast<uint64_t>(pow(10, y_pow + 1));
-                }
-                result += temp_res;
-                ++y_pow;
-                if (j == 0) {
-                    result += v_ume * static_cast<uint64_t>(pow(10, y_pow));
-                }
+    for (int i = x.size() - 1; i >= 0; --i) {
+        uint64_t v_ume = 0;
+        y_pow = 0;
+        for (int j = y.size() - 1; j >= 0; --j) {
+            v_ume *= pow(10, y_pow);
+
+            temp_res =
+                (static_cast<int>((y[j] - '0')) * static_cast<int>((x[i] - '0')) *
+                     static_cast<uint64_t>(pow(10, y_pow)) * static_cast<uint64_t>(pow(10, x_pow)) +
+                 v_ume);
+            if (temp_res >= 10) {
+                v_ume = static_cast<uint64_t>(
+                    floor(temp_res / static_cast<uint64_t>((pow(10, y_pow + 1)))));
+                temp_res %= static_cast<uint64_t>(pow(10, y_pow + 1));
             }
-            ++x_pow;
+            result += temp_res;
+            ++y_pow;
+            if (j == 0) {
+                result += v_ume * static_cast<uint64_t>(pow(10, y_pow));
+            }
         }
-
-        cout << x << " * " << y << " = " << result << "\n";
+        ++x_pow;
     }
 
-    //    uint64_t result = 0;
-    //
-    //    uint64_t temp_res;
-    //
-    //    uint64_t y_pow;
-    //    uint64_t x_pow = 0;
-    //
-    //    for (int i = x.size() - 1; i >= 0; --i) {
-    //        uint64_t v_ume = 0;
-    //        y_pow = 0;
-    //        for (int j = y.size() - 1; j >= 0; --j) {
-    //            v_ume *= pow(10, y_pow);
-    //
-    //            temp_res =
-    //                (static_cast<uint64_t>((y[j] - '0')) * static_cast<uint64_t>((x[i] - '0')) *
-    //                     static_cast<uint64_t>(pow(10, y_pow)) * static_cast<uint64_t>(pow(10,
-    //                     x_pow)) +
-    //                 v_ume);
-    //            if (temp_res >= 10) {
-    //                v_ume = static_cast<uint64_t>(
-    //                    floor(temp_res / static_cast<uint64_t>((pow(10, y_pow + 1)))));
-    //                temp_res %= static_cast<uint64_t>(pow(10, y_pow + 1));
-    //            }
-    //            result += temp_res;
-    //            ++y_pow;
-    //            if (j == 0) {
-    //                result += v_ume * static_cast<uint64_t>(pow(10, y_pow));
-    //            }
-    //        }
-    //        ++x_pow;
-    //    }
-    //
-    //    cout << x << " * " << y << " = " << result << "\n";
+    cout << result << "\n";
+}
+
+#include <vector>
+#include <iostream>
+
+// void multiplication() {
+//     std::string x, y;
+//     cin >> x >> y;
+//
+//     int x_length = x.size();
+//     int y_length = y.size();
+//     std::vector<int> result(x_length + y_length, 0);
+//
+//     for (int i = 0; i < y_length; ++i) {
+//         int v_ume = 0;
+//         for (int j = 0; j < x_length; ++j) {
+//             int xj = x[j] - '0';
+//             int yi = y[i] - '0';
+//             result[i + j - 1] += xj * yi + v_ume;
+//             v_ume  = result[i + j - 1] / 10;
+//             result[i + j - 1] %= 10;
+//         }
+//         result[i + y_length - 1] = v_ume;
+//     }
+//
+//     for(int i : result) {
+//         cout << i;
+//     }
+// }
+
+#include <vector>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+// Идея алгоритма взята отсюда: https://en.wikipedia.org/wiki/Multiplication_algorithm
+int main() {
+    std::string x, y;
+    cin >> x >> y;
+
+    int x_length = x.size();
+    int y_length = y.size();
+
+    if (x_length == 0 || y_length == 0) {
+        cout << "0";
+        return 0;
+    }
+
+    std::vector<int> result(x_length + y_length, 0);
+
+    int index1 = 0;
+    int index2;
+
+    for (int i = x_length - 1; i >= 0; --i) {
+        int v_ume = 0;
+        int x_num = x[i] - '0';
+        index2 = 0;
+        for (int j = y_length - 1; j >= 0; --j) {
+            int y_num = y[j] - '0';
+            int temp_res = x_num * y_num + result[index1 + index2] + v_ume;
+
+            v_ume = temp_res / 10;
+            result[index1 + index2] = temp_res % 10;
+
+            ++index2;
+        }
+
+        if (v_ume > 0) {
+            result[index1 + index2] += v_ume;
+        }
+        ++index1;
+    }
+
+    int index = result.size() - 1;
+    while (index >= 0 && result[index] == 0) {
+        --index;
+        if (index == -1) {
+            cout << "0";
+            return 0;
+        }
+    }
+
+    while (index >= 0) {
+        cout << result[index];
+        --index;
+    }
 
     return 0;
 }
