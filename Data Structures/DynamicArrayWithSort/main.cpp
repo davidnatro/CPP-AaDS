27657aed8bee2f27e4a953a6246b385e5a654b91
Lone Warden <davidnatro03@gmail.com>
2022-09-24 15:19:50 +0300

    vector func

--- Data Structures/DynamicArrayWithSort/main.cpp
+++ Data Structures/DynamicArrayWithSort/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iterator>
+#include <initializer_list>
 
 class Vector {
 public:
@@ -60,6 +62,10 @@ public:
             return *this;
         }
 
+        friend int64_t operator-(const Iterator &a, const Iterator &b) {
+            return a.m_ptr_ - b.m_ptr_;
+        }
+
         friend bool operator==(const Iterator &a, const Iterator &b) {
             return a.m_ptr_ == b.m_ptr_;
         }
@@ -73,20 +79,21 @@ public:
     };
 
     Vector() {
-        capacity_ = 0;
-        size_ = 0;
         data_ = new int[capacity_];
     }
 
     explicit Vector(int size) {
-        this->size_ = 0;
+        size_ = size;
         capacity_ = size * 2;
         data_ = new int[capacity_];
+        for (int i = 0; i < size_; ++i) {
+            data_[i] = 0;
+        }
     }
 
     Vector(const int *vals, int size) {
         size_ = size;
-        capacity_ = size;
+        capacity_ = size * 2;
         data_ = new int[capacity_];
         for (int i = 0; i < size; ++i) {
             data_[i] = vals[i];
@@ -98,13 +105,14 @@ public:
         capacity_ = vector.capacity_;
         data_ = new int[capacity_];
         for (int i = 0; i < size_; ++i) {
-            data_[i] = vector[i];
+            data_[i] = vector.data_[i];
         }
     }
 
     Vector(std::initializer_list<int> vals) {
         size_ = vals.size();
-        capacity_ = size_;
+        capacity_ = size_ * 2;
+        data_ = new int[capacity_];
         int index = 0;
         for (const int *it = vals.begin(); it != vals.end(); ++it) {
             data_[index] = *it;
@@ -116,22 +124,38 @@ public:
         return size_;
     }
 
-    bool isEmpty() const {
-        return size_ == 0;
-    }
-
     int getCapacity() const {
         return capacity_;
     }
 
+    bool isEmpty() const {
+        return size_ == 0;
+    }
+
     void resize(int new_size) {
-        int *new_arr = new int[new_size];
-        for (int i = 0; i < size_; ++i) {
-            new_arr[i] = data_[i];
+        int *new_arr;
+        if (new_size >= capacity_) {
+            capacity_ = 2 * new_size;
+            new_arr = new int[capacity_];
+        } else {
+            new_arr = new int[new_size];
+        }
+
+        if (new_size > size_) {
+            for (int i = size_; i < new_size; ++i) {
+                new_arr[i] = 0;
+            }
+
+            for (int i = 0; i < size_; ++i) {
+                new_arr[i] = data_[i];
+            }
+        } else {
+            for (int i = 0; i < new_size; ++i) {
+                new_arr[i] = data_[i];
+            }
         }
 
         size_ = new_size;
-        capacity_ = size_ * 2;
 
         delete[] data_;
         data_ = new_arr;
@@ -139,7 +163,6 @@ public:
 
     void pushBack(int value) {
         if (size_ >= capacity_) {
-            capacity_ *= 2;
             resize(capacity_);
         }
         data_[size_] = value;
@@ -147,9 +170,10 @@ public:
     }
 
     void popBack() {
-        if (size_ == 0) {
+        if (isEmpty()) {
             throw std::runtime_error("Empty Array!");
         }
+
         --size_;
     }
 
@@ -158,12 +182,11 @@ public:
     }
 
     void insert(int pos, int value) {
-        if (pos < 0 || pos >= capacity_) {
-            throw std::runtime_error("'Wrong Position!");
+        if (pos < 0 || pos > size_) {
+            throw std::runtime_error("Wrong Position!");
         }
 
         if (size_ >= capacity_) {
-            capacity_ *= 2;
             resize(capacity_);
         }
 
@@ -176,8 +199,10 @@ public:
     }
 
     void erase(int pos) {
-        if (pos < 0 || pos >= capacity_) {
-            throw std::runtime_error("'Wrong Position!");
+        if (isEmpty()) {
+            throw std::runtime_error("Empty Array!");
+        } else if (pos < 0 || pos >= size_) {
+            throw std::runtime_error("Wrong Position!");
         }
 
         for (int i = pos; i < size_ - 1; ++i) {
@@ -187,28 +212,33 @@ public:
     }
 
     int at(int pos) {
-        if (pos < 0 || pos >= capacity_) {
-            throw std::runtime_error("'Wrong Position!");
+        if (pos < 0 || pos >= size_) {
+            throw std::runtime_error("Wrong Position!");
         }
 
         return data_[pos];
     }
 
     int front() {
-        if (size_ == 0) {
+        if (isEmpty()) {
             throw std::runtime_error("Empty Array!");
         }
+
         return data_[0];
     }
 
     int back() {
-        if (size_ == 0) {
+        if (isEmpty()) {
             throw std::runtime_error("Empty Array!");
         }
         return data_[size_ - 1];
     }
 
     int &operator[](int pos) const {
+        if (pos < 0 || pos >= size_) {
+            throw std::runtime_error("Wrong Position!");
+        }
+
         return data_[pos];
     }
 
@@ -225,18 +255,10 @@ public:
     }
 
     Iterator begin() {
-        if (size_ == 0) {
-            throw std::runtime_error("Empty Array!");
-        }
-
         return Iterator(data_);
     }
 
     Iterator end() {
-        if (size_ == 0) {
-            throw std::runtime_error("Empty Array!");
-        }
-
         return Iterator(data_ + size_);
     }
 
@@ -246,84 +268,166 @@ public:
 
 private:
     int *data_;
-    int capacity_;
-    int size_;
+    int capacity_ = 10;
+    int size_ = 0;
 };
 
+Vector merge(const Vector::Iterator &begin, const Vector::Iterator &middle,
+             const Vector::Iterator &end) {
+    Vector vector(end - begin);
+    Vector::Iterator i = begin;
+    Vector::Iterator j = middle;
+    while (i != middle && j != end) {
+        if (*i <= *j) {
+            vector.pushBack(*i++);
+        } else {
+            vector.pushBack(*j++);
+        }
+    }
+
+    while (i != middle) {
+        vector.pushBack(*i++);
+    }
+
+    while (j != end) {
+        vector.pushBack(*j++);
+    }
+
+    return vector;
+}
+
 void mergeSort(Vector::Iterator begin, Vector::Iterator end) {
+    int64_t distance = end - begin;
+    if (distance <= 1) {
+        return;
+    }
+
+    Vector::Iterator middle = begin + distance / 2;
+
+    mergeSort(begin, middle);
+    mergeSort(middle, end);
+
+    Vector sorted = merge(begin, middle, end);
+
+    for (auto i : sorted) {
+        std::cout << i << "\t";
+    }
+    std::cout << "\n";
 }
 
 void insertionSort(Vector::Iterator begin, Vector::Iterator end) {
+    for (Vector::Iterator it = begin; it != end; ++it) {
+        for (Vector::Iterator jt = it + 1; jt != end; ++jt) {
+            if (*it > *jt) {
+                std::swap(*it, *jt);
+            }
+        }
+    }
 }
 
 using std::cout;
 
+void print(Vector vector) {
+    // cout << "size: " << vector.getSize() << " capacity: " << vector.getCapacity() << "\n";
+    for (Vector::Iterator it = vector.begin(); it != vector.end(); ++it) {
+        cout << *it << "\t";
+    }
+
+    cout << "\n";
+}
+
 int main() {
-    Vector vector(5);
-    cout << "size: " << vector.getSize() << "\n";
-    cout << "capacity: " << vector.getCapacity() << "\n";
-    for (int i = 0; i < 10; ++i) {
-        vector.pushBack(i);
+
+    int *arr = new int[10];
+    int ind = 0;
+    for (int i = 9; i >= 0; --i) {
+        arr[ind] = i;
+        ++ind;
     }
 
-    cout << "size: " << vector.getSize() << "\n";
-    cout << "capacity: " << vector.getCapacity() << "\n";
+    Vector vector(arr, 10);
 
-    for (int i = 0; i < vector.getSize(); ++i) {
-        cout << vector[i] << "\t";
-    }
+    cout << "initial: "
+         << "\n";
+    print(vector);
+    cout << "\n\n";
+
+    cout << "sorting..."
+         << "\n";
+
+    mergeSort(vector.begin(), vector.end());
+
     cout << "\n";
 
-    vector.popBack();
-    vector.popBack();
-    vector.popBack();
-
-    for (int i = 0; i < vector.getSize(); ++i) {
-        cout << vector[i] << "\t";
-    }
-    cout << "\n";
-    cout << "size: " << vector.getSize() << "\n";
-    cout << "capacity: " << vector.getCapacity() << "\n";
-
-    // vector.clear();
-    cout << "size: " << vector.getSize() << "\n";
-    cout << "capacity: " << vector.getCapacity() << "\n";
-    if (vector.getSize() == 0) {
-        cout << "empty"
-             << "\n";
-    } else {
-        for (int i = 0; i < vector.getSize(); ++i) {
-
-            cout << vector[i] << "\t";
-        }
-        cout << "\n";
-    }
-    cout << std::boolalpha << vector.isEmpty() << "\n";
-
-    for (auto it = vector.begin(); it != vector.end(); ++it) {
-        cout << *it << "\t";
-    }
-    cout << "\n";
-    vector.insert(3, 4);
-
-    for (auto it = vector.begin(); it != vector.end(); ++it) {
-        cout << *it << "\t";
-    }
-
-    cout << "\n";
-    vector.erase(4);
-    vector.erase(0);
-    vector.erase(2);
-    vector.insert(2, 3);
-    vector.popBack();
-    for (auto it = vector.begin(); it != vector.end(); ++it) {
-        cout << *it << "\t";
-    }
-    cout << "\n";
-
-    cout << vector.back() << "\n";
-    cout << vector.front() << "\n";
-    cout << vector.at(0) << "\n";
+    cout << "sorted: "
+         << "\n";
+    print(vector);
 
     return 0;
 }
+
+////     Vector vector(5);
+////    cout << "size: " << vector.getSize() << "\n";
+////    cout << "capacity: " << vector.getCapacity() << "\n";
+////    for (int i = 0; i < 10; ++i) {
+////        vector.pushBack(i);
+////    }
+////
+////    cout << "size: " << vector.getSize() << "\n";
+////    cout << "capacity: " << vector.getCapacity() << "\n";
+////
+////    for (int i = 0; i < vector.getSize(); ++i) {
+////        cout << vector[i] << "\t";
+////    }
+////    cout << "\n";
+////
+////    vector.popBack();
+////    vector.popBack();
+////    vector.popBack();
+////
+////    for (int i = 0; i < vector.getSize(); ++i) {
+////        cout << vector[i] << "\t";
+////    }
+////    cout << "\n";
+////    cout << "size: " << vector.getSize() << "\n";
+////    cout << "capacity: " << vector.getCapacity() << "\n";
+////
+////    // vector.clear();
+////    cout << "size: " << vector.getSize() << "\n";
+////    cout << "capacity: " << vector.getCapacity() << "\n";
+////    if (vector.getSize() == 0) {
+////        cout << "empty"
+////             << "\n";
+////    } else {
+////        for (int i = 0; i < vector.getSize(); ++i) {
+////
+////            cout << vector[i] << "\t";
+////        }
+////        cout << "\n";
+////    }
+////    cout << std::boolalpha << vector.isEmpty() << "\n";
+////
+////    for (auto it = vector.begin(); it != vector.end(); ++it) {
+////        cout << *it << "\t";
+////    }
+////    cout << "\n";
+////    vector.insert(3, 4);
+////
+////    for (auto it = vector.begin(); it != vector.end(); ++it) {
+////        cout << *it << "\t";
+////    }
+////
+////    cout << "\n";
+////    vector.erase(4);
+////    vector.erase(0);
+////    vector.erase(2);
+////    vector.insert(2, 3);
+////    vector.popBack();
+////    for (auto it = vector.begin(); it != vector.end(); ++it) {
+////        cout << *it << "\t";
+////    }
+////    cout << "\n";
+////
+////    cout << vector.back() << "\n";
+////    cout << vector.front() << "\n";
+////    cout << vector.at(0) << "\n";
