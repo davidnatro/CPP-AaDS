141c949e1f2d76274bd61c686733f5bbd367d347
David <davidnatro03@gmail.com>
2023-08-13 10:37:26 +0600

    remove duplicates from list

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
 #include "problems/medium/headers/longest-palindromic-substring.h"
-
 #include "test/runner/test-runner.h"
 
 int main() {
 
-    ITestRunner<int> *test_runner = new TestRunner<int>();
-
-    delete test_runner;
+    //    ITestRunner<int> *test_runner = new TestRunner<int>();
+    //
+    //    delete test_runner;
 
     return 0;
 }
