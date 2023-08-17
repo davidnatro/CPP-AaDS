0699e13d4655097d8fb57594bb56d3700d86bef0
David <davidnatro03@gmail.com>
2023-08-17 22:48:02 +0600

    intersection of two arrays 2

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "problems/easy/headers/intersection-of-two-arrays.h"
+#include "problems/easy/headers/intersection-of-two-arrays-ii.h"
 #include "problems/easy/headers/path-sum.h"
 #include "problems/medium/headers/longest-palindromic-substring.h"
 #include "test/runner/test-runner.h"
@@ -12,11 +12,11 @@ int main() {
     //
     //    delete test_runner;
 
-    std::vector<int> vec1{1, 2, 2, 1};
-    std::vector<int> vec2{2, 2};
+    std::vector<int> vec1{4, 9, 5};
+    std::vector<int> vec2{9, 4, 9, 8, 4};
 
-    IntersectionOfTwoArrays intersection_of_two_arrays;
-    for (const int element : intersection_of_two_arrays.intersection(vec1, vec2)) {
+    IntersectionOfTwoArraysII intersection_of_two_arrays;
+    for (const int element : intersection_of_two_arrays.intersect(vec1, vec2)) {
         std::cout << element << "\t";
     }
 
---
+++ LeetCode/problems/easy/headers/intersection-of-two-arrays-ii.h
@@ -0,0 +1,14 @@
+#ifndef LEETCODE_PROBLEMS_EASY_HEADERS_INTERSECTION_OF_TWO_ARRAYS_II_H_
+#define LEETCODE_PROBLEMS_EASY_HEADERS_INTERSECTION_OF_TWO_ARRAYS_II_H_
+
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+#include <map>
+#include <vector>
+
+class IntersectionOfTwoArraysII {
+public:
+    std::vector<int> intersect(std::vector<int> &nums1, std::vector<int> &nums2);
+};
+
+#endif
