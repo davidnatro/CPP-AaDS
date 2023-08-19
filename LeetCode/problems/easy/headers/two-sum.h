a0f912cf066a154f0daa8820c2f3ce6503d8eff3
David <davidnatro03@gmail.com>
2023-08-19 18:10:12 +0600

    two sum

---
+++ LeetCode/problems/easy/headers/two-sum.h
@@ -0,0 +1,12 @@
+#ifndef LEETCODE_PROBLEMS_EASY_HEADERS_TWO_SUM_H_
+#define LEETCODE_PROBLEMS_EASY_HEADERS_TWO_SUM_H_
+
+#include <map>
+#include <vector>
+
+class TwoSum {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target);
+};
+
+#endif
---
+++ LeetCode/problems/easy/sources/two-sum.cpp
@@ -0,0 +1,18 @@
+#include "../headers/two-sum.h"
+
+std::vector<int> TwoSum::twoSum(std::vector<int> &nums, int target) {
+    std::map<int, int> map;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        map[nums[i]] = i;
+    }
+
+    for (int i = 0; i < nums.size(); ++i) {
+        int j = target - nums[i];
+        if (map.count(j) && map[j] != i) {
+            return std::vector<int>{map[j], i};
+        }
+    }
+
+    return std::vector<int>{};
+}
