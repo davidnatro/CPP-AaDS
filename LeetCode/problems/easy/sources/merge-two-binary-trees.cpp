b97f1849907cf6a2d1f939025117c59b342cf830
David <davidnatro03@gmail.com>
2023-08-19 17:29:35 +0600

    merge two binary trees

--- LeetCode/problems/easy/sources/merge-two-binary-trees.cpp
+++ LeetCode/problems/easy/sources/merge-two-binary-trees.cpp
@@ -13,3 +13,21 @@ TreeNode::~TreeNode() {
     delete left;
     delete right;
 }
+
+TreeNode *MergeTwoBinaryTrees::mergeTrees(TreeNode *root1, TreeNode *root2) {
+    if (root1 == nullptr) {
+        return root2;
+    }
+
+    if (root2 == nullptr) {
+        return root1;
+    }
+
+    TreeNode *head = new TreeNode();
+
+    head->val = root1->val + root2->val;
+    head->left = mergeTrees(root1->left, root2->left);
+    head->right = mergeTrees(root1->right, root2->right);
+
+    return head;
+}
