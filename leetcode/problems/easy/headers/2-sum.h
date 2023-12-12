edffdbce8b4ced89eedcb1cec6d6cd9293837639
David Natro <davidnatro03@gmail.com>
2023-12-12 21:29:44 +0600

    valid sudoku

---
+++ leetcode/problems/easy/headers/2-sum.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_PROBLEMS_EASY_HEADERS_2_SUM_H_
+#define LEETCODE_PROBLEMS_EASY_HEADERS_2_SUM_H_
+
+// https://leetcode.com/problems/two-sum/
+
+#include <vector>
+
+class TwoSum {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target);
+};
+
+#endif
