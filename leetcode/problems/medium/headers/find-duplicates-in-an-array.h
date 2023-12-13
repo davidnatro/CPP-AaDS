f1ecb009f90c9bba8ea9831c3359628154651508
David Natro <davidnatro03@gmail.com>
2023-12-13 22:54:58 +0600

    FindDuplicatesInAnArray

---
+++ leetcode/problems/medium/headers/find-duplicates-in-an-array.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_PROBLEMS_MEDIUM_HEADERS_FIND_DUPLICATES_IN_AN_ARRAY_H_
+#define LEETCODE_PROBLEMS_MEDIUM_HEADERS_FIND_DUPLICATES_IN_AN_ARRAY_H_
+
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+#include <vector>
+
+class FindDuplicatesInAnArray {
+public:
+    std::vector<int> findDuplicates(const std::vector<int>& nums);
+};
+
+#endif
