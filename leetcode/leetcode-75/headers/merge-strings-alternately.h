e3fff173fda6eb5a365b19461d7eb8efa2cda62d
David Natro <davidnatro03@gmail.com>
2023-12-16 12:09:23 +0600

    leetcode75 easy tasks

---
+++ leetcode/leetcode-75/headers/merge-strings-alternately.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_PROBLEMS_EASY_HEADERS_MERGE_STRINGS_ALTERNATELY_H_
+#define LEETCODE_PROBLEMS_EASY_HEADERS_MERGE_STRINGS_ALTERNATELY_H_
+
+// https://leetcode.com/problems/merge-strings-alternately/
+
+#include <string>
+
+class MergeStringsAlternately {
+public:
+    std::string mergeAlternately(std::string word1, std::string word2);
+};
+
+#endif
