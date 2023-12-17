b9b0111f9ef86bfe79496bf40f9ea8c20aa986d8
David Natro <davidnatro03@gmail.com>
2023-12-17 17:49:25 +0600

    gcd of strings

---
+++ leetcode/leetcode-75/headers/gcd-of-strings.h
@@ -0,0 +1,14 @@
+#ifndef LEETCODE_PROBLEMS_EASY_HEADERS_GCD_OF_STRINGS_H_
+#define LEETCODE_PROBLEMS_EASY_HEADERS_GCD_OF_STRINGS_H_
+
+// https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
+#include <numeric>
+#include <string>
+
+class GcdOfStrings {
+public:
+    std::string gcdOfStrings(std::string str1, std::string str2);
+};
+
+#endif
