d5239b1fa0a0da311683bf7488fd4e8ae26127e5
David Natro <davidnatro03@gmail.com>
2023-12-18 11:07:36 +0600

    refactored leetcode75 dirs

---
+++ leetcode/leetcode-75/array-string/headers/can-place-flowers.h
@@ -0,0 +1,11 @@
+#ifndef LEETCODE_LEETCODE_75_ARRAY_STRING_HEADERS_CAN_PLACE_FLOWERS_H_
+#define LEETCODE_LEETCODE_75_ARRAY_STRING_HEADERS_CAN_PLACE_FLOWERS_H_
+
+#include <vector>
+
+class CanPlaceFlowers {
+public:
+    bool canPlaceFlowers(const std::vector<int>& flowerbed, int n);
+};
+
+#endif
