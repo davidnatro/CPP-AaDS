e1880453647e964935b793f8a54f562c8f7d1b7a
David Natro <davidnatro03@gmail.com>
2023-12-20 21:29:57 +0600

    product of array except self

---
+++ leetcode/leetcode-75/array-string/headers/product-of-array-except-self.h
@@ -0,0 +1,11 @@
+#ifndef LEETCODE_LEETCODE_75_ARRAY_STRING_HEADERS_PRODUCT_OF_ARRAY_EXCEPT_SELF_H_
+#define LEETCODE_LEETCODE_75_ARRAY_STRING_HEADERS_PRODUCT_OF_ARRAY_EXCEPT_SELF_H_
+
+#include <vector>
+
+class ProductOfArrayExceptSelf {
+public:
+    std::vector<int> productExceptSelf(const std::vector<int>& nums) const;
+};
+
+#endif
---
+++ leetcode/leetcode-75/array-string/sources/product-of-array-except-self.cpp
@@ -0,0 +1,24 @@
+#include "../headers/product-of-array-except-self.h"
+
+std::vector<int> ProductOfArrayExceptSelf::productExceptSelf(const std::vector<int> &nums) const {
+    if (nums.size() < 2) {
+        return nums;
+    }
+
+    std::vector<int> result(nums.size(), 1);
+
+    result[0] = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+        result[i] = nums[i] * result[i - 1];
+    }
+
+    int right_product = nums[nums.size() - 1];
+    result[result.size() - 1] = result[result.size() - 2];
+    for (int i = nums.size() - 2; i > 0; --i) {
+        result[i] = result[i - 1] * right_product;
+        right_product *= nums[i];
+    }
+    result[0] = right_product;
+
+    return result;
+}
