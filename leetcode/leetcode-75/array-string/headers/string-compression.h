40b97970688935523980fb3b90c6237251aea304
David Natro <davidnatro03@gmail.com>
2023-12-21 23:11:23 +0600

    string compression

---
+++ leetcode/leetcode-75/array-string/headers/string-compression.h
@@ -0,0 +1,11 @@
+#ifndef LEETCODE_LEETCODE_75_ARRAY_STRING_HEADERS_STRING_COMPRESSION_H_
+#define LEETCODE_LEETCODE_75_ARRAY_STRING_HEADERS_STRING_COMPRESSION_H_
+
+#include <vector>
+
+class StringCompression {
+public:
+    int compress(std::vector<char>& chars);
+};
+
+#endif
