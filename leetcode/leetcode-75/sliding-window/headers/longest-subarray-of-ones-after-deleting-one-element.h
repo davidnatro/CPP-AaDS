85d628bd8e1b43a01d13e27de47fe05aa1d2320b
David Natro <davidnatro03@gmail.com>
2023-12-30 13:08:21 +0600

    sliding window tasks

---
+++ leetcode/leetcode-75/sliding-window/headers/longest-subarray-of-ones-after-deleting-one-element.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_LEETCODE_75_SLIDING_WINDOW_HEADERS_LONGEST_SUBARRAY_OF_ONES_AFTER_DELETING_ONE_ELEMENT_H_
+#define LEETCODE_LEETCODE_75_SLIDING_WINDOW_HEADERS_LONGEST_SUBARRAY_OF_ONES_AFTER_DELETING_ONE_ELEMENT_H_
+
+// https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+
+#include <vector>
+
+class LongestSubarrayOfOnesAfterDeletingOneElement {
+public:
+    int longestSubarray(const std::vector<int>& nums) const;
+};
+
+#endif
