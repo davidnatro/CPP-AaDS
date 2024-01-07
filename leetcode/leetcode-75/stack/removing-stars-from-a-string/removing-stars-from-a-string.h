964c64318ac283bb97242eb646795632b10b9c29
David Natro <davidnatro03@gmail.com>
2024-01-07 12:00:40 +0600

    removing stars from a string

---
+++ leetcode/leetcode-75/stack/removing-stars-from-a-string/removing-stars-from-a-string.h
@@ -0,0 +1,14 @@
+#ifndef LEETCODE_LEETCODE_75_STACK_REMOVING_STARS_FROM_A_STRING_REMOVING_STARS_FROM_A_STRING_H_
+#define LEETCODE_LEETCODE_75_STACK_REMOVING_STARS_FROM_A_STRING_REMOVING_STARS_FROM_A_STRING_H_
+
+#include <sstream>
+#include <stack>
+#include <string>
+
+class RemovingStarsFromAString {
+
+public:
+    std::string removeStars(const std::string &s) const;
+};
+
+#endif
