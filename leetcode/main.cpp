7d16cd553691aec11183160426f32f841ea0c24c
David Natro <davidnatro03@gmail.com>
2024-05-21 21:03:27 +0500

    trie

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "leetcode-75/trie/search-suggestions-system/search-suggestions-system.hpp"
 
 int main() {
+  SearchSuggestionsSystem search_suggestions_system;
+  auto result =
+      search_suggestions_system.suggestedProducts({ "mobile", "mouse", "moneypot", "monitor",
+                                                    "mousepad" }, "mouse");
+
+  for (const std::vector<std::string> &row : result) {
+    for (const std::string &column : row) {
+      std::cout << column << "\t";
+    }
+    std::cout << "\n";
+  }
 
   return 0;
 }
