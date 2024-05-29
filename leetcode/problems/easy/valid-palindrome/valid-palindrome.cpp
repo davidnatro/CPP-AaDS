e7f5980f865c9314fbe83bbc7ba38e72043dcfbd
David Natro <davidnatro03@gmail.com>
2024-05-30 00:22:43 +0500

    optimized valid palindrome

--- leetcode/problems/easy/valid-palindrome/valid-palindrome.cpp
+++ leetcode/problems/easy/valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,21 @@
 #include "valid-palindrome.hpp"
 
 bool ValidPalindrome::isPalindrome(std::string s) const {
-  auto remove = std::remove_if(s.begin(), s.end(), [&](const char item) {
-    return !iswalnum(item);
-  });
-  s.erase(remove, s.end());
-  for (char &letter : s) {
-    letter = tolower(letter);
-  }
-
   int left = 0;
   int right = s.size() - 1;
 
   while (left <= right) {
-    if (s[left] != s[right]) {
+    if (!iswalnum(s[left])) {
+      left += 1;
+      continue;
+    }
+
+    if (!iswalnum(s[right])) {
+      right -= 1;
+      continue;
+    }
+
+    if (tolower(s[left]) != tolower(s[right])) {
       return false;
     }
 
