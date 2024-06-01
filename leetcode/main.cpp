9c487cb2601e8b0f0939ac2bc73ada4d5fb64d26
David Natro <davidnatro03@gmail.com>
2024-06-02 02:42:38 +0500

    permutation in string

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "problems/medium/permutation-in-string/permutation-in-string.hpp"
 
 int main() {
+  PermutationInString permutation_in_string;
+  std::cout << std::boolalpha << permutation_in_string.checkInclusion("ab", "eidbaooo");
   return 0;
 }
