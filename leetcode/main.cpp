e1f46760f7499f5af7120225874cdb5718e50168
David Natro <davidnatro03@gmail.com>
2024-06-13 19:18:03 +0500

    find min

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include "problems/medium/permutation-in-string/permutation-in-string.hpp"
 
 int main() {
-  PermutationInString permutation_in_string;
-  std::cout << std::boolalpha << permutation_in_string.checkInclusion("ab", "eidbaooo");
   return 0;
 }
