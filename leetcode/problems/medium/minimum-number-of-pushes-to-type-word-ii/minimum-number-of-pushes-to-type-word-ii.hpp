5e492aae440fd0681b9d8f35aab7f51650fecab2
David Natro <davidnatro03@gmail.com>
2024-08-06 22:50:23 +0500

    daily task

---
+++ leetcode/problems/medium/minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.hpp
@@ -0,0 +1,14 @@
+#ifndef MINIMUM_NUMBER_OF_PUSHES_TO_TYPE_WORD_II_HPP
+#define MINIMUM_NUMBER_OF_PUSHES_TO_TYPE_WORD_II_HPP
+
+// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+
+#include <string>
+#include <vector>
+
+class MinimumNumberOfPushesToTypeWordII {
+public:
+    int minimumPushes(std::string word) const;
+};
+
+#endif
