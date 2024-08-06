05bad0d924b63fd1703caa24849a05c5c24060ed
David Natro <davidnatro03@gmail.com>
2024-08-07 00:32:24 +0500

    top interview 150 best time to buy and sell stock 2

---
+++ leetcode/top-interview-150/array-string/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.hpp
@@ -0,0 +1,13 @@
+#ifndef BEST_TIME_TO_BUY_AND_SELL_STOCK_II_HPP
+#define BEST_TIME_TO_BUY_AND_SELL_STOCK_II_HPP
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+#include <vector>
+
+class BestTimeToBuyAndSellStockII {
+public:
+    int maxProfit(const std::vector<int>& prices) const;
+};
+
+#endif
